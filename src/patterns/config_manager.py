1331771f16212c4dd3f15e643047685b00509427
Euan Bourke <30806087+Bourkupine@users.noreply.github.com>
2024-11-20 15:21:33 +0000

    Singleton: Added Config Manager for managing the configs (#1)
    * config: created config manager
    
    Singleton pattern!!
    
    ---------
    
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>

---
+++ src/patterns/config_manager.py
@@ -0,0 +1,18 @@
+from config import config
+
+'''
+Singleton Pattern:
+'''
+
+class ConfigManager:
+    instance = None
+
+    def __new__(cls):
+        # If an instance doesn't exist, create a new one
+        if cls.instance is None:
+            cls.instance = super(ConfigManager, cls).__new__(cls)
+        return cls.instance
+
+    # Return a config
+    def get_config(self, key):
+        return getattr(config, key, None)
