9d8f1dff9298a347e7a90dddb525bb2ffda40bd3
Euan Bourke <euanbourke@gmail.com>
2024-11-20 14:29:30 +0000

    config: simplified removing the internal dictionary
    opted for just using the config file for configs and not using an
    internal one. Will be slower but I dont think its necessary.
    
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>

--- src/config/config_manager.py
+++ src/config/config_manager.py
@@ -14,27 +14,16 @@ class ConfigManager:
             cls.instance = super(ConfigManager, cls).__new__(cls)
         return cls.instance
 
-    def __init__(self):
-        if not hasattr(self, 'init'):
-            self.init = True
-            self.config - self.load_config_file()
-
-    # Load in all configs from our module as a dictionary
-    def load_config_file(self):
-        return {
-            key: getattr(config, key) for key in dir(config)
-        }
-
     # Set a config
     def set_config(self, key, val):
-        self.config[key] = val
+        setattr(config, key, val)
+        # Reload the config after editing it
         config.reload_config()
 
     # Return a config
     def get_config(self, key):
-        return self.config[key]
+        return getattr(config, key, None)
 
     # Reload our config file after making a change
     def reload_config(self):
         importlib.reload(config)
-        self.config = self.load_config_file()
