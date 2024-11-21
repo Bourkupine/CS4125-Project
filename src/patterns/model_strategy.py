e7c87089efb69c14fa27de198d05d82b4bb9402d
Euan Bourke <euanbourke@gmail.com>
2024-11-21 13:58:21 +0000

    strategy: implemented randomforest strategy
    also renamed to model_strategy.py
    
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>

---
+++ src/patterns/model_strategy.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+'''
+Strategy Pattern:
+'''
+
+class ModelStrategyInterface(ABC):
+
+    '''
+    This is an abstract method for training our model
+    '''
+    @abstractmethod
+    def train(self, X_train, y_train):
+        pass
+
+    '''
+    This is an abstract method for predicting using our model
+    '''
+    @abstractmethod
+    def predict(self, X_test):
+        pass
