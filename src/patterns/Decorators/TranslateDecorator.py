83428b284788c1d633995b4e99d63ffc17ef6c34
Euan Bourke <euanbourke@gmail.com>
2024-11-24 01:08:57 +0000

    config: fixed import issues
    Signed-off-by: Euan Bourke <euanbourke@gmail.com>

--- src/patterns/Decorators/TranslateDecorator.py
+++ src/patterns/Decorators/TranslateDecorator.py
@@ -1,6 +1,6 @@
 from turtle import pd
 from pandas import DataFrame, read_csv
-from src.config.config import Config
+from src.patterns.config_manager import ConfigManager
 from src.data.Preprocessor import Preprocessor
 from src.utils.translate import trans_to_en
 
@@ -8,11 +8,15 @@ class TranslateDecorator(Preprocessor):
 
     def __init__(self, PreProcessing):
         self.PreProcessing = PreProcessing
+        self.config_manager = ConfigManager
 
     def preprocess(self,df: DataFrame):
         #translating using trans_to_en
         self.PreProcessing.preprocess()
-        df[Config.TICKET_SUMMARY] = pd.Series(trans_to_en(df[Config.TICKET_SUMMARY].to_list()), index=df.index)
-        df[Config.INTERACTION_CONTENT] = pd.Series(trans_to_en(df[Config.INTERACTION_CONTENT].to_list()),
+
+        ticket_summary = self.config_manager.get_config("TICKET_SUMMARY")
+        interaction_content = self.config_manager.get_config("INTERACTION_CONTENT")
+        df[ticket_summary] = pd.Series(trans_to_en(df[ticket_summary].to_list()), index=df.index)
+        df[interaction_content] = pd.Series(trans_to_en(df[interaction_content].to_list()),
                                                    index=df.index)
         return df
